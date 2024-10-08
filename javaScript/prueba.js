341da68df6171e05ba83094919611dac376cf30c
CarlosJavier1993 <carlos.ramos-c@uniminuto.edu.co>
2024-10-08 10:08:33 -0500

    probando PAT

---
+++ javaScript/prueba.js
@@ -0,0 +1,17 @@
+//variables
+let nombre="Hola mundo";
+let nombreCompleto; //cameCase
+const pi= 3.1416;
+console.log(nombre);
+console.log(pi);
+
+//Tipos de datos
+
+let num=5;
+let texto="Hola";
+let verdadero=true;
+let falso=false;
+let a;
+console.log (typeof num);
+console.log(typeof texto);
+console.log(typeof verdadero);
